cee2e258b5288c766dab60042484918e18176c64
hjun-dev <2738qwe@gmail.com>
2026-01-05 05:35:56 +0000

    Deploying to gh-pages from @ hjun-dev/hjun-dev.github.io@eafd477885fddc78c670729a099fc9bbb3679156 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/2026-01-05-convex-intro/";
-          
-        },
-      },{id: "post-unified-approach-optimization-to-deep-rl",
+        },{id: "post-unified-approach-optimization-to-deep-rl",
         
           title: "Unified Approach: Optimization to Deep RL",
         
