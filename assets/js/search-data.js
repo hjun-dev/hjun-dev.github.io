fb5e0dc16482319fdffdf722b36252ad933c6e98
hjun-dev <2738qwe@gmail.com>
2026-01-24 09:50:15 +0000

    Deploying to gh-pages from @ hjun-dev/hjun-dev.github.io@3ff50248f8fdb3276b7dd99ed3bf16362bdbfab1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-convex-optimization-15-barrier-method",
+        },{id: "post-convex-optimization-16-primal-dual-interior-point-methods",
+        
+          title: "[Convex Optimization] 16. Primal-dual interior-point methods",
+        
+        description: "Inequality and equality constrained problem을 풀기 위한 second-order method 중 하나인 Barrier Method 정의 및 분석",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/00-primal-dual-interior-point-method/";
+          
+        },
+      },{id: "post-convex-optimization-15-barrier-method",
         
           title: "[Convex Optimization] 15. Barrier Method",
         
